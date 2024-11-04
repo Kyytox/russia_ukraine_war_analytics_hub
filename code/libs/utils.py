5a5a6cf6a79ce619c3b1d5e2ef1d894bb0aaafd5
Kyytox <kytox.dev@protonmail.com>
2024-11-04 04:34:09 +0100

    add function

--- code/libs/utils.py
+++ code/libs/utils.py
@@ -2,6 +2,28 @@ import os
 import pandas as pd
 
 
+def get_telegram_accounts(path):
+    """
+    Get telegram accounts
+
+    Args:
+        path: path to accounts
+
+    Returns:
+        List of accounts
+    """
+
+    # get accounts
+    list_accounts = [
+        file_name.split(".")[0]
+        for file_name in os.listdir(path)
+        if file_name.endswith(".parquet")
+    ]
+    print(f"Accounts: {list_accounts}")
+
+    return list_accounts
+
+
 def read_data(base_path, file_name):
     """
     Read data from parquet
@@ -25,7 +47,7 @@ def read_data(base_path, file_name):
     return df
 
 
-def save_data(base_path, file_name, df_old, df):
+def save_data(base_path, file_name, df_old=None, df=None):
     """
     Save data to parquet
 
@@ -45,8 +67,40 @@ def save_data(base_path, file_name, df_old, df):
 
     print(f"Saving {df.shape} data to {base_path}/{file_name}.parquet")
 
+    # concat data
+    if df_old is not None:
+        df = (
+            pd.concat([df_old, df])
+            .drop_duplicates()
+            .reset_index(drop=True)
+            .sort_values("date")
+        )
+
     # save data to parquet
-    df = pd.concat([df_old, df]).drop_duplicates().reset_index(drop=True)
     df.to_parquet(os.path.join(base_path, f"{file_name}.parquet"))
 
     return
+
+
+def keep_data_to_process(df_source, df_to_filter):
+    """
+    Filter data
+
+    Args:
+        df_source: dataframe
+        df_to_filter: dataframe to filter
+
+    Returns:
+        Dataframe with filtered data
+    """
+
+    # keep data not in clean data
+    if df_to_filter.empty:
+        df = df_source
+    else:
+        df = df_source[
+            ~df_source["id_message"].isin(df_to_filter["id_message"])
+        ].reset_index(drop=True)
+    print(f"Data to process: {df.shape}")
+
+    return df
