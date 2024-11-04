ac25c11c9be79579d345ffad4a4839391c7b7076
Kyytox <kytox.dev@protonmail.com>
2024-11-04 04:34:43 +0100

    add prrocess filter incidents railway

---
+++ code/process/filter/job_filter.py
@@ -0,0 +1,10 @@
+# Fonction
+from process.filter.incidents_railway import filter_incidents_railway
+
+
+def job_filter():
+    """
+    Process filter
+    """
+    # filter incidents railway
+    filter_incidents_railway()
