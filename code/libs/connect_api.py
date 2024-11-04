58e927876647d9077d93a1734396a5379ae26767
Kyytox <kytox.dev@protonmail.com>
2024-11-04 04:34:21 +0100

    chagne path name

--- code/libs/connect_api.py
+++ code/libs/connect_api.py
@@ -3,7 +3,7 @@ import yaml
 from telethon import TelegramClient
 
 # Variables
-from utils.variables import credentials_path
+from utils.variables import path_creds_telegram
 
 
 def telegram_connect():
@@ -14,7 +14,7 @@ def telegram_connect():
         - client: TelegramClient
     """
     # get credentials
-    with open(credentials_path) as file:
+    with open(path_creds_telegram) as file:
         credentials = yaml.safe_load(file)
 
     # Connect to Telegram
