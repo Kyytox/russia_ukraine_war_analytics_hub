2f476a2158fd5cdc2eddccdb8a89652a61e4057b
Kyytox <kytox.dev@protonmail.com>
2025-01-04 06:12:19 +0100

    add streamlit gestion/class app

---
+++ streamlit_gestion/app.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+mod = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+if mod not in sys.path:
+    sys.path.append(mod)
+
+from core.config.paths import (
+    PATH_TELEGRAM_TRANSFORM,
+    PATH_TWITTER_CLEAN,
+    PATH_FILTER_SOCIAL_MEDIA,
+)
