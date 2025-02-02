23ecff9eb03b5a78040c9351b6513f4141b8dd6d
Kyytox <kytox.dev@protonmail.com>
2025-02-02 06:48:25 +0100

    upd main with new names and flow

--- main.py
+++ main.py
@@ -1,93 +1,69 @@
-import os
 import sys
-import time
 
-from prefect import flow
 
 # Process Telegram
-from core.process_social_network.telegram.telegram_extract import job_telegram_extract
-from core.process_social_network.telegram.telegram_cleaning import job_telegram_cleaning
-from core.process_social_network.telegram.telegram_transform import (
-    job_telegram_transform,
+from core.process_datalake.telegram.telegram_extract import flow_telegram_extract
+from core.process_datalake.telegram.telegram_cleaning import flow_telegram_cleaning
+from core.process_datalake.telegram.telegram_transform import (
+    flow_telegram_transform,
 )
-from core.process_social_network.telegram.telegram_filter import job_telegram_filter
+from core.process_datalake.telegram.telegram_filter import flow_telegram_filter
 
 # Process Twitter
-from core.process_social_network.twitter.twitter_extract import job_twitter_extract
-from core.process_social_network.twitter.twitter_cleaning import job_twitter_cleaning
-from core.process_social_network.twitter.twitter_filter import job_twitter_filter
+from core.process_datalake.twitter.twitter_extract import flow_twitter_extract
+from core.process_datalake.twitter.twitter_cleaning import flow_twitter_cleaning
+from core.process_datalake.twitter.twitter_filter import flow_twitter_filter
 
 # Filter to classify
-from core.process_social_network.filter.social_media_filter import (
-    job_social_media_filter,
+from core.process_datalake.filter.datalake_filter import (
+    flow_datalake_filter,
 )
-from core.process_social_network.pre_classify.social_media_pre_classify import (
-    job_social_media_pre_classify,
+from core.process_datalake.qualification.datalake_qualif import (
+    flow_datalake_qualif,
 )
-from core.process_social_network.classify.filter_to_classify import (
-    job_filter_to_classify,
+from core.process_datalake.classify.classify_to_cloud import (
+    flow_classify_to_cloud,
 )
 
 # Classify to Excel Final
-from core.process_social_network.excel_final.classify_to_excel_final import (
-    job_classify_to_excel_final,
+from core.process_datalake.excel_final.classify_to_excel_final import (
+    flow_classify_to_excel_final,
 )
 
-# Process Applicatifs
-from core.process_applicatifs.extract.extract_incidents_railway import (
-    job_extract_incident_railway,
-)
-from core.process_applicatifs.transform.transform_incidents_railway import (
-    job_transform_incident_railway,
-)
-from core.process_applicatifs.datamarts.datamarts_incidents_railway import (
-    job_datamarts_incidents_railway,
-)
+# # Process Applicatifs
+from core.process_data_warehouse.flow_dwh_inc_railway import flow_dwh_inc_railway
 
 
 def process_telegram():
     """
     Process Telegram data
     """
-    job_telegram_extract()
+    # flow_telegram_extract()
 
-    job_telegram_cleaning()
+    # flow_telegram_cleaning()
 
-    job_telegram_transform()
+    flow_telegram_transform()
 
-    job_telegram_filter()
+    # flow_telegram_filter()
 
 
-@flow(name="PROCESS TWITTER")
 def process_twitter():
     """
     Process Twitter data
     """
-    job_twitter_extract()
+    flow_twitter_extract()
 
-    job_twitter_cleaning()
+    flow_twitter_cleaning()
 
-    job_twitter_filter()
+    # flow_twitter_filter()
 
 
-def process_filter_classify():
+def process_dwh():
     """
-    Process Filter to Classify
-    """
-
-    job_social_media_filter()
-
-
-def process_applicatifs():
-    """
-    Process Applicatifs data
+    Process Data Warehouse
     """
 
-    job_extract_incident_railway()
-
-    job_transform_incident_railway()
-
-    job_datamarts_incidents_railway()
+    flow_dwh_inc_railway()
 
 
 def main():
@@ -98,7 +74,7 @@ def main():
         print("tg")
         print("tw")
         print("filt")
-        print("pre")
+        print("qual")
         print("class")
         print("app")
 
@@ -112,15 +88,15 @@ def main():
             elif param == "tw":
                 process_twitter()
             elif param == "filt":
-                process_filter_classify()
-            elif param == "pre":
-                job_social_media_pre_classify()
+                flow_datalake_filter()
+            elif param == "qual":
+                flow_datalake_qualif()
             elif param == "class":
-                job_filter_to_classify()
+                flow_classify_to_cloud()
             elif param == "excel":
-                job_classify_to_excel_final()
-            elif param == "app":
-                process_applicatifs()
+                flow_classify_to_excel_final()
+            elif param == "dwh":
+                process_dwh()
             else:
                 print("Invalid parameter")
                 print("List of parameters:")
