50195ff168b9bf7ad01d99892090c6286de584f3
Kyytox <kytox.dev@protonmail.com>
2025-02-11 19:56:57 +0100

    add dict correspondance cols for DWH

---
+++ core/config/dwh_corresp_schema.py
@@ -0,0 +1,33 @@
+CORRESP_SCHEMA_INCIDENT_RAILWAY = {
+    "Date": "date",
+    "Region": "region",
+    "Location": "location",
+    "Gps": "gps",
+    "Damaged Equipment": "dmg_eqp",
+    "Incident Type": "inc_type",
+    "Collision With": "coll_with",
+    "Locomotive Damaged": "nb_loco_dmg",
+    "Relay Damaged": "nb_relay_dmg",
+    "Partisans Group": "prtsn_grp",
+    "Partisans Arrest": "prtsn_arr",
+    "Partisans Names": "prtsn_names",
+    "Partisans Age": "prtsn_age",
+    "Applicable Laws": "app_laws",
+    "Source Links": "source_links",
+    "Locomotive Damaged": "nb_loco_dmg",
+    "Relay Damaged": "nb_relay_dmg",
+    #
+    "Sabotage Success": "sabotage_success",
+    "Comments": "comments",
+    "Exact Date": "exact_date",
+}
+
+
+CORRESP_SCHEMA_RUSSIA_BLOCK_SITES = {
+    "Domain": "domain",
+    "Banning Authority": "banning_authority",
+    "Locale": "country_domain",
+    "Type of site": "category",
+    "Subcategory": "subcategory",
+    "Date blocked": "date_blocked",
+}
