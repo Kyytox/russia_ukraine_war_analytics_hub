3eb6f85356a79c00ba25ce061d44cf733844f4eb
Kyytox <kytox.dev@protonmail.com>
2025-02-16 09:10:33 +0100

    add dmt_by_date_by_metrics, dmt_by_authority_category, dmt_by_category_over_time, update dmt_by_date

--- core/process_data_warehouse/datamarts/datamarts_ru_block_sites.py
+++ core/process_data_warehouse/datamarts/datamarts_ru_block_sites.py
@@ -22,7 +22,8 @@ def dmt_global(df: pd.DataFrame) -> pd.DataFrame:
     Create datamart for number of blocked sites by different columns
     """
 
-    list_metrics = ["country_domain", "category", "subcategory", "banning_authority"]
+    # list_metrics = ["country_domain", "category", "subcategory", "banning_authority"]
+    list_metrics = ["country_domain", "subcategory", "category", "banning_authority"]
     final_df = pd.DataFrame()
 
     for metric in list_metrics:
@@ -55,33 +56,165 @@ def dmt_by_date(df: pd.DataFrame) -> pd.DataFrame:
         df (pd.DataFrame): Dataframe with datamart by date
     """
 
-    daily_counts = (
-        df.groupby("date_blocked").size().reset_index(name="sites_blocked_daily")
+    # By Day
+    df_day = (
+        df.groupby("date_blocked")
+        .size()
+        .reset_index(name="count")
+        .rename(columns={"date_blocked": "date"})
     )
 
-    monthly_counts = (
-        df.groupby("month").size().reset_index(name="sites_blocked_monthly")
-    )
-    yearly_counts = df.groupby("year").size().reset_index(name="sites_blocked_yearly")
-
-    merged_df = daily_counts
-    merged_df = merged_df.merge(
-        monthly_counts,
-        left_on=merged_df["date_blocked"].dt.to_period("M"),
-        right_on="month",
-        how="left",
-    )
-    merged_df = merged_df.merge(
-        yearly_counts,
-        left_on=merged_df["date_blocked"].dt.year,
-        right_on="year",
-        how="left",
+    # add 1,2,3 january 2022
+    df_day = pd.concat(
+        [
+            df_day,
+            pd.DataFrame(
+                {
+                    "date": pd.date_range(start="2022-01-01", end="2022-01-03"),
+                    "count": 0,
+                }
+            ),
+        ],
+        ignore_index=True,
     )
 
-    # fill nan values
-    merged_df = merged_df.fillna(0)
+    # add absent days
+    df_day = df_day.set_index("date").resample("D").asfreq().reset_index()
+    df_day["count"] = df_day["count"].fillna(0).astype(int)
+    df_day["type_metric"] = "by_day"  # add col type_metrics
+    print(df_day)
 
-    return merged_df
+    # By Month
+    df_month = (
+        df.groupby("month")
+        .size()
+        .reset_index(name="count")
+        .rename(columns={"month": "date"})
+    )
+
+    # add absent months
+    # convert to period
+    df_month["date"] = df_month["date"].dt.to_period("M")
+    df_month = df_month.set_index("date").resample("M").asfreq().reset_index()
+    df_month["count"] = df_month["count"].fillna(0).astype(int)
+    df_month["date"] = df_month["date"].dt.to_timestamp()
+    df_month["type_metric"] = "by_month"  # add col type_metrics
+
+    # By Year
+    df_year = (
+        df.groupby("year")
+        .size()
+        .reset_index(name="count")
+        .rename(columns={"year": "date"})
+    )
+    df_year["date"] = pd.to_datetime(df_year["date"], format="%Y")
+    df_year["type_metric"] = "by_year"  # add col type_metrics
+
+    # By day cumul
+    df_day_cumul = df_day.copy()
+    df_day_cumul["count"] = df_day_cumul["count"].cumsum()
+    df_day_cumul["type_metric"] = "by_day_cumul"
+
+    # concat
+    df_concat = pd.concat(
+        [
+            df_day,
+            df_month,
+            df_year,
+            df_day_cumul,
+        ],
+        ignore_index=True,
+    )
+    return df_concat
+
+
+@task(name="dmt-by-date-by-metrics", task_run_name="dmt-by-date_by-metrics")
+def dmt_by_date_by_metrics(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Create datamart for number of blocked sites by date and metrics
+    - Day
+    - Month
+    - Year
+    - Metrics
+
+    ** month and year are already in the dataframe
+
+    Args:
+        df (pd.DataFrame): Dataframe to control
+
+    Returns:
+        df (pd.DataFrame): Dataframe with datamart by date and metrics
+    """
+
+    df_final = pd.DataFrame()
+    list_metrics = ["banning_authority", "country_domain"]
+
+    # range date
+    df_date = pd.DataFrame(
+        {
+            "month": pd.date_range(start="2022-01-01", end="2024-12-31", freq="MS"),
+        }
+    )
+
+    for metric in list_metrics:
+        df_tmp = df.groupby(["month", metric]).size().reset_index(name="count")
+        df_tmp.rename(columns={metric: "metric"}, inplace=True)
+
+        # merge with date (for missing values)
+        df_tmp = df_date.merge(df_tmp, on="month", how="left")
+
+        df_tmp["type_metric"] = f"by_{metric}"
+        df_tmp["count"] = df_tmp["count"].fillna(0).astype(int)
+
+        # replcae metric None by "Not specified"
+        if metric == "country_domain":
+            df_tmp["metric"] = df_tmp["metric"].replace({None: "Unknown"})
+        else:
+            df_tmp["metric"] = df_tmp["metric"].replace({np.nan: "Not specified"})
+
+        # concat
+        df_final = pd.concat([df_final, df_tmp], ignore_index=True)
+
+    return df_final
+
+
+@task(name="dmt-by-authority-category", task_run_name="dmt-by-authority-category")
+def dmt_by_authority_category(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Create datamart for number of blocked sites by authority and category
+
+    Args:
+        df (pd.DataFrame): Dataframe to control
+
+    Returns:
+        df (pd.DataFrame): Dataframe with datamart by authority and category
+    """
+
+    df_final = pd.crosstab(df["banning_authority"], df["category"]).reset_index()
+    df_final = pd.melt(
+        df_final, id_vars=["banning_authority"], var_name="category", value_name="count"
+    )
+    # print(df_final)
+
+    return df_final
+
+
+@task(name="dmt-by-category-over-time", task_run_name="dmt-by-category-over-time")
+def dmt_by_category_over_time(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Create datamart for number of blocked sites by category over time
+
+    Args:
+        df (pd.DataFrame): Dataframe to control
+
+    Returns:
+        df (pd.DataFrame): Dataframe with datamart by category over time
+    """
+
+    df_final = df.groupby(["month", "category"]).size().unstack(fill_value=0)
+    # print(df_final)
+
+    return df_final
 
 
 @flow(
@@ -99,7 +232,8 @@ def flow_dmt_russia_blocked_sites():
     print("df")
     print(df)
 
-    df["month"] = df["month"].dt.to_period("M")
+    # df["month"] = df["month"].dt.to_period("M")
+    # df["year"] = df["year"].dt.to_period("Y")
 
     # Cont Global
     df_tmp = dmt_global(df)
@@ -109,6 +243,21 @@ def flow_dmt_russia_blocked_sites():
     # By Date
     df_tmp = dmt_by_date(df)
     path = f"{PATH_DMT_RU_BLOCK_SITES}/count_by_date.parquet"
+    df_tmp.to_parquet(path, index=False)
+
+    # By date and metrics
+    df_tmp = dmt_by_date_by_metrics(df)
+    path = f"{PATH_DMT_RU_BLOCK_SITES}/count_by_date_and_metrics.parquet"
+    df_tmp.to_parquet(path, index=False)
+
+    # By Authorithy and Category
+    df_tmp = dmt_by_authority_category(df)
+    path = f"{PATH_DMT_RU_BLOCK_SITES}/count_by_authority_category.parquet"
+    df_tmp.to_parquet(path, index=False)
+
+    # Block by Category over time
+    df_tmp = dmt_by_category_over_time(df)
+    path = f"{PATH_DMT_RU_BLOCK_SITES}/count_by_category_over_time.parquet"
     df_tmp.to_parquet(path, index=False)
 
     if df.empty:
