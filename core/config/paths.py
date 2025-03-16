5141322dfd968e3ce08232dc2986b61e8b3c2008
Kyytox <kytox.dev@protonmail.com>
2025-03-16 22:19:11 +0100

    update connexion to google service

--- core/config/paths.py
+++ core/config/paths.py
@@ -8,8 +8,7 @@ PATH_ROOT = "/mnt/4046B55946B55080/russia_ukraine_war_analytics_hub"
 
 # credentials
 PATH_CREDS_API = "core/utils/credentials.yaml"
-PATH_CREDS_GOOGLE_SHEET = "core/utils/credentials.json"
-PATH_TOKEN_GOOGLE_SHEET = "core/utils/token.json"
+PATH_SERVICE_ACCOUNT = "core/utils/service_account.json"
 
 # data Telegram
 PATH_TELEGRAM_RAW = "data/datalake/telegram/raw"
@@ -34,16 +33,15 @@ PATH_CLASSIFY_DATALAKE = "data/datalake/classify"
 
 # Data Wharehouse
 PATH_DWH_SOURCES = "data/data_warehouse/sources"
-# PATH_DWH_TRANSFORM = "data/data_warehouse/transform"
 
 # Data Marts
-# PATH_DMT_INC_RAILWAY = "data/datamarts/incidents_railway"
 PATH_DMT_INC_RAILWAY = "data/data_warehouse/datamarts/incidents_railway"
 PATH_DMT_RU_BLOCK_SITES = "data/data_warehouse/datamarts/russia_block_sites"
+PATH_DMT_COMPO_WEAPONS = "data/data_warehouse/datamarts/compo_weapons"
 
 # Path Utils
-# PATH_JSON_RU_REGION = "core/utils/russia_region.json"
 PATH_JSON_RU_REGION = "core/utils/ru_region.json"
+PATH_COUNTRY_ISO = "core/config/countries_iso.json"
 
 # scripts
 PATH_SCRIPT_SERVICE_OLLAMA = "core/utils/script_service.sh"
--- core/libs/google_api.py
+++ core/libs/google_api.py
@@ -1,18 +1,14 @@
-import os.path
 import pandas as pd
 
 from prefect import task
 
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from google.oauth2 import service_account
 
 # Variables
 from core.config.paths import (
-    PATH_CREDS_GOOGLE_SHEET,
-    PATH_TOKEN_GOOGLE_SHEET,
+    PATH_SERVICE_ACCOUNT,
 )
 
 
@@ -24,33 +20,12 @@ def connect_google_sheet_api():
     Returns:
         service: Google Sheet service
     """
-
     SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
-    creds = None
 
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists(PATH_TOKEN_GOOGLE_SHEET):
-        creds = Credentials.from_authorized_user_file(PATH_TOKEN_GOOGLE_SHEET, SCOPES)
-
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        print("No valid credentials")
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                PATH_CREDS_GOOGLE_SHEET, SCOPES
-            )
-            creds = flow.run_local_server(port=0)
-
-        # Save the credentials for the next run
-        with open(PATH_TOKEN_GOOGLE_SHEET, "w") as token:
-            token.write(creds.to_json())
-
-    # connect to google sheet
-    service = build("sheets", "v4", credentials=creds)
+    credentials = service_account.Credentials.from_service_account_file(
+        PATH_SERVICE_ACCOUNT, scopes=SCOPES
+    )
+    service = build("sheets", "v4", credentials=credentials)
 
     return service
 
