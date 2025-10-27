34791ad26f9f112271cad93b1d19a4ff73058a1b
Kyytox <kytox.dev@protonmail.com>
2025-10-27 01:13:29 +0100

    add ineractive maps

--- viz_app/pages/interactive_maps.py
+++ viz_app/pages/interactive_maps.py
@@ -1,8 +1,5 @@
-import pandas as pd
-
 import dash
-from dash import Input, Output, callback, html, dcc
-from utils.variables import PATH_DMT_BLOCK_SITE
+from dash import html
 
 from assets.components.cards import create_card
 
@@ -10,14 +7,11 @@ from assets.components.cards import create_card
 dash.register_page(__name__)
 
 
-dmt_global = pd.read_parquet(f"{PATH_DMT_BLOCK_SITE}/dmt_global.parquet")
-
-
 page_content = {
     "card1": {
         "title": "DeepState Map of Ukraine",
         "text": "News of russia's war against Ukraine on the map.",
-        "image": None,
+        "image": "./assets/images/img_map_card1.png",
         "url": "https://deepstatemap.live/en#6/50.0571388/32.7612305",
         "tags": ["Map"],
         "color_tags": ["#121096"],
@@ -25,7 +19,7 @@ page_content = {
     "card2": {
         "title": "UA Control Map",
         "text": "UA Control And Units Map.",
-        "image": None,
+        "image": "./assets/images/img_map_card2.png",
         "url": "https://www.google.com/maps/d/u/0/viewer?mid=1xPxgT8LtUjuspSOGHJc2VzA5O5jWMTE&ll=47.50751714509709%2C34.14477250205766&z=6",
         "tags": ["Map", "Units"],
         "color_tags": ["#121096", "#aaa824"],
@@ -33,7 +27,7 @@ page_content = {
     "card3": {
         "title": "War Mapper",
         "text": "Understand the impact of the frontline changes in Ukraine through a series of control charts.",
-        "image": None,
+        "image": "./assets/images/img_map_card3.png",
         "url": "https://www.warmapper.org/",
         "tags": ["Map", "Graphs"],
         "color_tags": ["#121096", "#4abb15"],
@@ -41,16 +35,56 @@ page_content = {
     "card4": {
         "title": "Eyes on Russia",
         "text": "Map draws on the database of videos, photos, satellite imagery or other media related to Russia’s invasion of Ukraine.",
-        "image": None,
+        "image": "./assets/images/img_map_card4.png",
         "url": "https://eyesonrussia.org/",
         "tags": ["Map"],
         "color_tags": ["#121096"],
     },
     "card5": {
-        "title": "Russian Military Objects Are in Range of ATACMS",
+        "title": "ISW: Russian Military Objects Are in Range of ATACMS",
         "text": "Map allows users to inspect 225 known Russian military objects in Russia that are in range of Ukrainian ATACMS.",
-        "image": None,
-        "url": "https://storymaps.arcgis.com/stories/8b060c46ee6f49908f9fb415ad23051c",
+        "image": "./assets/images/img_map_card5.png",
+        "url": "https://understandingwar.org/analysis/map-room/?_search_research=ATACMS&_teams=russia-ukraine",
+        "tags": ["Map"],
+        "color_tags": ["#121096"],
+    },
+    "card6": {
+        "title": "ISW: Interactive Map: Russia's Invasion of Ukraine",
+        "text": "Interactive maps complements the static control-of-terrain maps that ISW daily produces with high-fidelity and, where possible, street level assessments of the war in Ukraine.",
+        "image": "./assets/images/img_map_card6.png",
+        "url": "https://understandingwar.org/analysis/map-room/?_search_research=control&_teams=russia-ukraine",
+        "tags": ["Map"],
+        "color_tags": ["#121096"],
+    },
+    "card7": {
+        "title": "Ukraine Conflict Monitor",
+        "text": "ACLED’s Ukraine Conflict Monitor provides near real-time information on the ongoing war, including an interactive map, data file, weekly situation updates. Designed to help researchers, policymakers, media, and the wider public track key conflict developments in Ukraine.",
+        "image": "./assets/images/img_map_card7.png",
+        "url": "https://acleddata.com/monitor/ukraine-conflict-monitor",
+        "tags": ["Map", "Data Sources"],
+        "color_tags": ["#121096", "#14aca4"],
+    },
+    "card8": {
+        "title": "Air Raid Alert Map of Ukraine",
+        "text": "Showcases air raid alerts across Ukraine on an interactive map, providing real-time updates on siren activations and safety information.",
+        "image": "./assets/images/img_map_card8.png",
+        "url": "https://alerts.in.ua/en",
+        "tags": ["Map", "Analytics"],
+        "color_tags": ["#121096", "#961010"],
+    },
+    "card9": {
+        "title": "Ukraine Daily Updates",
+        "text": "Map maintained by Andrew Perpetua, providing the daily events of this war and the current situation on the battlefield. Notable events affecting military targets and civilian victims, fortifications and much more is shown through the map’s adjustable layers.",
+        "image": "./assets/images/img_map_card9.png",
+        "url": "https://map.ukrdailyupdate.com/?lat=49.385949&lng=32.744751&z=7&d=20384&c=1&l=0",
+        "tags": ["Map"],
+        "color_tags": ["#121096"],
+    },
+    "card10": {
+        "title": "Timeline of Invasion",
+        "text": "Map by David Batashvili, visualize the movements of armies during Russia's invasion of Ukraine from February 24, 2022.",
+        "image": "./assets/images/img_map_card10.png",
+        "url": "https://www.google.com/maps/d/u/0/embed?mid=1lscRK6ehG0l2V-XvJ16nsyblMsQ&ll=48.32028801617995%2C38.452502603277026&z=8",
         "tags": ["Map"],
         "color_tags": ["#121096"],
     },
